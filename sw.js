859284269a8f70f3c1d0122b91dbd08de9ccdfe6
ErWoo11 <latribu_woo11@hotmail.com>
2025-11-20 16:10:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-const CACHE_NAME = 'caducano-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './agregar.html',
-  './medicamentos.html',
-  './logo.png',
-  './manifest.json'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('caducano-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/agregar.html',
+        '/medicamentos.html',
+        '/logo.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => {
+      return r || fetch(e.request);
+    })
   );
 });
